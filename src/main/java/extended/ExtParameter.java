112f7309dc420b9ee2c9fae98815f97022fc035a
Boris Marin <borismarin@gmail.com>
2014-12-29 15:55:43 +0000

    Removed commented code (extpar factory)

--- src/main/java/extended/ExtParameter.java
+++ src/main/java/extended/ExtParameter.java
@@ -1,22 +1,9 @@
 package extended;
 
-import javax.xml.bind.annotation.XmlRegistry;
 import javax.xml.bind.annotation.XmlTransient;
 
-import org.lemsml.model.ObjectFactory;
 import org.lemsml.model.Parameter;
 
 @XmlTransient
 public class ExtParameter extends Parameter {
-
-
 }
-
-//@XmlRegistry
-//class ObjectFactoryEx extends ObjectFactory {
-//	@Override
-//	public
-//	ExtParameter createParameter() {
-//		return new ExtParameter();
-//	}
-//}
