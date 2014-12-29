37feee15d0e58d5d47f914097b93ffbd60714faf
Adrian Quintana <adrian.perez@ucl.ac.uk>
2014-12-29 14:31:27 +0000

    Added extended object factory for unmarshalling

--- src/test/java/org/lemsml/model/test/ValidationUnmarshallingTest.java
+++ src/test/java/org/lemsml/model/test/ValidationUnmarshallingTest.java
@@ -4,9 +4,11 @@
 package org.lemsml.model.test;
 
 import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.net.URL;
+import java.util.List;
 
 import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
@@ -20,6 +22,9 @@ import org.lemsml.model.ComponentType;
 import org.lemsml.model.Lems;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
+
+import extended.ExtObjectFactory;
+import extended.ExtParameter;
 
 /**
  * @author boris
@@ -41,21 +46,21 @@ public class ValidationUnmarshallingTest {
 
 			Unmarshaller unmarshaller = jc.createUnmarshaller();
 			unmarshaller.setSchema(schema);
+			unmarshaller.setProperty("com.sun.xml.internal.bind.ObjectFactory",new ExtObjectFactory());
 			Lems lems = (Lems) unmarshaller.unmarshal(new File(hr_lems.getFile()));
-			System.out.println(lems);
 			ComponentType hrct = lems.getComponentType().get(0);
 			String desc = hrct.getDescription();
-			assert(desc.equals("     The Hindmarsh Rose model is a simplified point cell model which     captures complex firing patterns of single neurons, such as     periodic and chaotic bursting. It in a fast spiking subsystem,     which is a generalization of the Fitzhugh-Nagumo system, coupled     to a slower subsystem which allows the model to fire bursts. The     dynamical variables x,y,z correspond to the membrane potential, a     recovery variable, and a slower adaptation current, respectively.     "));
-
-
+			assertEquals(desc, "     The Hindmarsh Rose model is a simplified point cell model which     captures complex firing patterns of single neurons, such as     periodic and chaotic bursting. It in a fast spiking subsystem,     which is a generalization of the Fitzhugh-Nagumo system, coupled     to a slower subsystem which allows the model to fire bursts. The     dynamical variables x,y,z correspond to the membrane potential, a     recovery variable, and a slower adaptation current, respectively.     ");
+			List<ExtParameter> extParameterList = hrct.getParameter();
+			assertEquals(extParameterList.get(0).getDescription(), "cubic term in x         nullcline");
 		}
 		catch(SAXParseException ex){
 			fail("Test file " + hr_lems.getPath() + "not valid according to schema " + lems_xsd.getPath());
 		} catch (SAXException e) {
 			fail("Problems with schema file" + lems_xsd.getPath());
 		} catch (JAXBException e) {
-			fail("Problems unmarshalling file" + hr_lems.getPath());
 			e.printStackTrace();
+			fail("Problems unmarshalling file" + hr_lems.getPath());
 		}
 	}
 
