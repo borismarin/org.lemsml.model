32b0e4d304732eb640291d16922c60686bab686d
Adrian Quintana <adrian.perez@ucl.ac.uk>
2014-12-29 18:42:02 +0000

    Added xml transformation based on xlst

--- src/test/java/org/lemsml/model/test/ValidationUnmarshallingTest.java
+++ src/test/java/org/lemsml/model/test/ValidationUnmarshallingTest.java
@@ -14,6 +14,13 @@ import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 
@@ -38,8 +45,29 @@ public class ValidationUnmarshallingTest {
 				.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 		URL lems_xsd = getClass().getResource("/Schemas/LEMS_v0.8.0.xsd");
 		URL hr_lems = getClass().getResource("/examples/HindmarshRose3d.xml");
+		URL canonical_xslt = getClass().getResource("/Schemas/canonical.xslt");
+//		URL hr_lems_transformed = getClass().getResource("/examples/output.xml");
+		
 
 		try {
+			TransformerFactory factory = TransformerFactory.newInstance();
+	        Source xslt = new StreamSource(new File(canonical_xslt.getFile()));
+	        Transformer transformer;
+			try {
+				transformer = factory.newTransformer(xslt);
+				Source text = new StreamSource(new File(hr_lems.getFile()));
+				transformer.transform(text, new StreamResult(new File("output.xml")));
+			} catch (TransformerConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TransformerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}    
+	        
+	        
+//			URL hr_lems_transformed = getClass().getResource("/examples/output.xml");
+			
 			Schema schema = sf.newSchema(new File(lems_xsd.getFile()));
 
 			JAXBContext jc = JAXBContext.newInstance(Lems.class);
@@ -47,7 +75,7 @@ public class ValidationUnmarshallingTest {
 			Unmarshaller unmarshaller = jc.createUnmarshaller();
 			unmarshaller.setSchema(schema);
 			unmarshaller.setProperty("com.sun.xml.internal.bind.ObjectFactory",new ExtObjectFactory());
-			Lems lems = (Lems) unmarshaller.unmarshal(new File(hr_lems.getFile()));
+			Lems lems = (Lems) unmarshaller.unmarshal(new File("output.xml"));
 			ComponentType hrct = lems.getComponentType().get(0);
 			String desc = hrct.getDescription();
 			assertEquals(desc, "     The Hindmarsh Rose model is a simplified point cell model which     captures complex firing patterns of single neurons, such as     periodic and chaotic bursting. It in a fast spiking subsystem,     which is a generalization of the Fitzhugh-Nagumo system, coupled     to a slower subsystem which allows the model to fire bursts. The     dynamical variables x,y,z correspond to the membrane potential, a     recovery variable, and a slower adaptation current, respectively.     ");
