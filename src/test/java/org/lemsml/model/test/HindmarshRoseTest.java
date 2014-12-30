eceac7d002998c2bdb9fd774a0c6590b4246d07f
Boris Marin <borismarin@gmail.com>
2014-12-30 20:37:03 +0000

    Improving and refactoring tests. And yes, I LIKE static methods.

--- src/test/java/org/lemsml/model/test/HindmarshRoseTest.java
+++ src/test/java/org/lemsml/model/test/HindmarshRoseTest.java
@@ -1,6 +1,7 @@
 package org.lemsml.model.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -19,9 +20,11 @@ import extended.ExtParameter;
  */
 public class HindmarshRoseTest{
 
-	private LemsTestUtils hr_compdef;
-	private LemsTestUtils hr_sim;
-	private File canonical_xslt;
+	private LemsXmlUtils utils;
+	private File schema;
+	private File hr_comptype;
+	private File hr_sim;
+	private File hr_noncanon_sim;
 
 	private File getLocalFile(String fname) {
 		return new File(getClass().getResource(fname).getFile());
@@ -29,43 +32,46 @@ public class HindmarshRoseTest{
 
 	@Before
 	public void setUp() {
-		File schema = getLocalFile("/Schemas/LEMS_v0.8.0.xsd");
-		canonical_xslt = getLocalFile("/Schemas/canonical.xslt");
-		hr_compdef = new LemsTestUtils(
-				getLocalFile("/examples/HindmarshRose3d.xml"), schema);
-		hr_sim = new LemsTestUtils(
-				getLocalFile("/examples/Run_Chaotic_HindmarshRose.xml"), schema);
+		schema = getLocalFile("/Schemas/LEMS_v0.8.0.xsd");
+		hr_comptype = getLocalFile("/examples/HindmarshRose3d.xml");
+		hr_sim = getLocalFile("/examples/Run_Chaotic_HindmarshRose.xml");
+		hr_noncanon_sim = getLocalFile("/examples/NonCanon_Run_Chaotic_HindmarshRose.xml");
 	}
 
 	@Test
 	public void validateComponent() {
-		assertTrue(hr_compdef.validate());
+		assertTrue(XmlFileUtils.validate(hr_comptype, schema));
 	}
 
 	@Test
 	public void validateSimulation() {
-		assertTrue(hr_sim.validate());
+		assertTrue(XmlFileUtils.validate(hr_sim, schema));
 	}
 	
 	@Test
-	public void applyXSLT(){
-		//TODO: Add assert
-		hr_compdef.applyXSLT(canonical_xslt);
+	public void testCanonicalize(){
+		File xslt = getLocalFile("/Schemas/canonical.xslt");
+		System.out.println("Asserting that a noncanonical file fails to validate...");
+		assertFalse(XmlFileUtils.validate(hr_noncanon_sim, schema));
+
+		System.out.println("Asserting that the canonicalized version validates...");
+		File transformed = XmlFileUtils.transform(hr_noncanon_sim, xslt);
+		assertTrue(XmlFileUtils.validate(transformed, schema));
 	}
 	
 	
 	@Test
 	public void testUnmarshallingComponent() {
 
-		Lems lems = hr_compdef.unmarshall();
+		Lems lems = LemsXmlUtils.unmarshall(hr_comptype, schema);
 		ComponentType hrct = lems.getComponentType().get(0);
 
 		String desc = hrct.getDescription();
 		assertEquals(
 				desc,
 				"     The Hindmarsh Rose model is a simplified point cell model which     captures complex firing patterns of single neurons, such as     periodic and chaotic bursting. It in a fast spiking subsystem,     which is a generalization of the Fitzhugh-Nagumo system, coupled     to a slower subsystem which allows the model to fire bursts. The     dynamical variables x,y,z correspond to the membrane potential, a     recovery variable, and a slower adaptation current, respectively.     ");
-		List<ExtParameter> extParameterList = hrct.getParameter();
 
+		List<ExtParameter> extParameterList = hrct.getParameter();
 		assertEquals(extParameterList.get(0).getDescription(),
 				"cubic term in x         nullcline");
 	}
