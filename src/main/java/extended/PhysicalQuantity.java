b5bdb596e185214ea2ce104cde5293b448f1e329
Boris Marin <borismarin@gmail.com>
2015-01-16 20:33:35 +0000

    Added Physical Quantity type and parser.

---
+++ src/main/java/extended/PhysicalQuantity.java
@@ -0,0 +1,30 @@
+package extended;
+
+
+public class PhysicalQuantity {
+
+    public Float value;
+    public String unit;
+
+	public Float getValue() {
+		return value;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public void setValue(Float value) {
+		this.value = value;
+	}
+	
+	@Override
+	public String toString() {
+		return "PhysicalQuantity [value=" + value + ", unit=" + unit + "]";
+	}
+
+}
--- src/test/java/org/lemsml/model/test/IncludesVisitorTest.java
+++ src/test/java/org/lemsml/model/test/IncludesVisitorTest.java
@@ -33,7 +33,7 @@ public class IncludesVisitorTest extends BaseTest {
 		LemsParser parser = new LemsParser(include0, schema);
 		parser.processIncludes();
 		assertEquals(parser.getLems().getConstant().size(),3);
-
+        System.out.println(parser.getLems().getConstant().get(0).getValue());
 	}
 
 
