359de753fb67d77f6168df0797f757362ace89d7
Boris Marin <borismarin@gmail.com>
2015-02-02 14:01:09 +0000

    Added extended.Unit.

---
+++ src/main/java/extended/Unit.java
@@ -0,0 +1,26 @@
+package extended;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+/**
+ * @author borismarin
+ *
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+public class Unit extends org.lemsml.model.Unit
+{
+
+	private javax.measure.Unit<?> unit;
+
+	public javax.measure.Unit<?> getUnit()
+	{
+		return this.unit;
+	}
+
+	public void setUnit(javax.measure.Unit<?> unit)
+	{
+		this.unit = unit;
+	}
+
+}
