3ff453c96a88cd23b04ed3ba3fc67eb371140b6f
borismarin <borismarin@gmail.com>
2015-03-31 20:11:17 +0100

    Still playing with test scaffolding for expr resolution...

--- src/test/java/org/lemsml/model/test/ExpressionResolverTest.java
+++ src/test/java/org/lemsml/model/test/ExpressionResolverTest.java
@@ -1,14 +1,12 @@
 package org.lemsml.model.test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.lemsml.model.compiler.parser.LEMSParser;
-import org.lemsml.model.compiler.parser.XMLUtils;
+import org.lemsml.model.compiler.LEMSCompilerFrontend;
 import org.lemsml.model.extended.Lems;
 
 public class ExpressionResolverTest extends BaseTest {
@@ -20,18 +18,17 @@ public class ExpressionResolverTest extends BaseTest {
 		schema = getLocalFile("/Schemas/LEMS_v0.9.0.xsd");
 	}
 
-
 	@Test
 	public void testNested() throws Throwable {
 
 		File lemsDoc = getLocalFile("/examples/expression-resolver-test/nested_expressions.xml");
 
-		LEMSParser parser = new LEMSParser(lemsDoc, schema);
-		assertTrue(XMLUtils.validate(lemsDoc, schema));
-		Lems parsedLems = parser.parse();
+		LEMSCompilerFrontend compiler = new LEMSCompilerFrontend(lemsDoc,
+				schema);
+		Lems compiledLems = compiler.generateLEMSDocument();
 
-		System.out.println(parsedLems.getConstants().get(0).getValue());
-		assertEquals(-0.1, parsedLems.getConstants().get(0).getValue().getValue(), 1e-6);
+		assertEquals(-0.1, compiledLems.getConstantByName("const0").getValue()
+				.getValue(), 1e-6);
 	}
 
 }
