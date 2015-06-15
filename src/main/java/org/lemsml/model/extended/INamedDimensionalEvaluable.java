175131f9ecb88ef27cf0b0f68f160ec3aef2b601
borismarin <borismarin@gmail.com>
2015-06-15 16:58:21 +0100

    Residual comments.

--- src/main/java/org/lemsml/model/extended/INamedDimensionalEvaluable.java
+++ src/main/java/org/lemsml/model/extended/INamedDimensionalEvaluable.java
@@ -1,6 +1,4 @@
 package org.lemsml.model.extended;
 
 public interface INamedDimensionalEvaluable extends IDimensionalEvaluable, INamed{
-	//public IDimensionalEvaluable getDimensionalValue();
-	//public void setDimensionalValue(IDimensionalEvaluable val) throws UndefinedParameterException;
 }
